942e01789f183fa1901030b994ac8b2f3533d130
Kyle Sanderson <kyle.leet@gmail.com>
2025-08-10 13:54:54 -0700

    feat(debug): add pprof endpoint (#29)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"strings"
@@ -218,6 +219,14 @@ func runServer() {
 		}
 	}()
 
+	// Start profiling server
+	go func() {
+		log.Info().Msg("Starting pprof server on :6060")
+		log.Info().Msg("Access profiling at: http://localhost:6060/debug/pprof/")
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Error().Err(err).Msg("Profiling server failed")
+		}
+	}()
 	// Wait for interrupt signal to gracefully shutdown the server
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
