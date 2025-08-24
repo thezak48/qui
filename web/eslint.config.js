2b092b035ad879a466c5fac089884c6321db5b38
soup <s0up4200@pm.me>
2025-08-25 00:41:58 +0300

    chore(lint): add eslint rules (#81)
    Co-authored-by: martyluky <35452459+martylukyy@users.noreply.github.com>

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -19,5 +20,18 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/quotes': ['warn', 'double'],
+      '@stylistic/comma-dangle': ['warn', 'always'],
+      '@stylistic/multiline-ternary': ['warn', 'never'],
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/indent': ['error', 2, { 'SwitchCase': 1 }],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'linebreak-style': ['error', 'unix'],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
   },
 ])
